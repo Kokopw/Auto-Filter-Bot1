103f0ce267c03e638aff5217ed3d77bbb504a4ac
Kokopw <kmelon175@gmail.com>
2025-01-25 15:43:00 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -368,7 +368,7 @@ async def save_shortlink(client, message):
     try:
         _, url, api = message.text.split(" ", 2)
     except:
-        return await message.reply_text("<b>Command Incomplete:-\n\ngive me a shortlink & api along with the command...\n\nEx:- <code>/shortlink mdisklink.link 5843c3cc645f5077b2200a2c77e0344879880b3e</code>")   
+        return await message.reply_text("<b>Command Incomplete:-\n\ngive me a shortlink & api along with the command...\n\nEx:- <code>/shortlink adsfly.in ee5868654d787db9ddd2352a5c0151b15a1a2ea6</code>")   
     try:
         await get_shortlink(url, api, f'https://t.me/{temp.U_NAME}')
     except:
